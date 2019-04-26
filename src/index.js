bf31f903adff0e45798595aed09463406075bf5f
whj768702 <whj768702@gmail.com>
2019-04-26 08:49:00 +0800

    add Square Board and Game component.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,82 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    };
+  }
+  render() {
+    return (
+      <button className="square" onClick={() => this.props.onClick()}>
+      {this.props.value}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sqaures: Array(9).fill(null)
+    };
+  }
+  handleClick(i) {
+    const squares = this.state.sqaures.slice();
+    squares[i] = 'X';
+    this.setState({sqaures: squares});
+  }
+  renderSquare(i) {
+    return (
+      <Square value={this.state.sqaures[i]} onClick={()=>this.handleClick(i)} />
+    );
+  }
+
+  render() {
+    const status = 'next player: x';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board/>
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <div>{/* TODO */}</div>
+        </div>
+      </div> 
+    );
+  }
+}
+ReactDOM.render(<Game />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
