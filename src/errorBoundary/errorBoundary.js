84548ece75ab272128806765961c7c10d299d43c
whj768702 <whj768702@gmail.com>
2019-05-16 09:40:52 +0800

    feat: react中错误边界。

---
+++ src/errorBoundary/errorBoundary.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hasError: false
+    }
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true }
+  }
+
+  componentDidCatch(error, info) {
+    console.log(error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // 出错后返回的展示UI
+      return <h1>Something went wrong.</h1>
+    }
+    
+    return this.props.children;
+  }
+}
