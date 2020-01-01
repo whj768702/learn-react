000ed5b35379391658e9cff55ec65059e665cc90
whj768702 <whj768702@gmail.com>
2020-01-01 18:06:22 +0800

    feat(hooks): useRef

--- src/hooks/useRef/useRef.js
+++ src/hooks/useRef/useRef.js
@@ -1,28 +1,30 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, {useRef, useState, useEffect} from 'react';
 
-function useRefDemo () {
-    const inputEl = useRef(null);
-    const onButtonClick = () => {
-        inputEl.current.value = 'hello world!';
-        console.log(inputEl);
-    };
+function useRefDemo() {
+  const inputEl = useRef(null);
+  const onButtonClick = () => {
+    inputEl.current.value = 'hello world!';
+    console.log(inputEl);
+  };
 
-    const [text, setText] = useState('ce shi');
-    const textRef = useRef(null);
+  const [text, setText] = useState('ce shi');
+  const textRef = useRef(null);
 
-    useEffect(() => {
-        textRef.current = text;
-        console.log(textRef);
-    });
-    return (
-        <>
-            <input type="text" ref={inputEl}/>
-            <button onClick={onButtonClick}>在input上展示文字</button>
-            <br/>
-            <br/>
-            <input type="text" value={text} onChange={(e) => {setText(e.target.value);}}/>
-        </>
-    );
+  useEffect(() => {
+    textRef.current = text;
+    console.log('terxtRef.current: ', textRef.current);
+  });
+  return (
+    <>
+      <input type="text" ref={inputEl}/>
+      <button onClick={onButtonClick}>在input上展示文字</button>
+      <br/>
+      <br/>
+      <input type="text" value={text} onChange={(e) => {
+        setText(e.target.value);
+      }}/>
+    </>
+  );
 }
 
 export default useRefDemo;
