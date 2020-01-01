a38b738c63580174e79c42b31e7fc3f082b0c12b
whj768702 <whj768702@gmail.com>
2020-01-01 16:22:54 +0800

    feat: 调整侧边栏。

--- src/homePage.jsx
+++ src/homePage.jsx
@@ -6,6 +6,10 @@ import MyApp from "./welcome/welcome";
 import useStateDemo from "./hooks/useState/useState";
 import useMemoDemo from "./hooks/useMemo/useMemo";
 import './index.css';
+import {Clock, MyClock} from "./clock/clock";
+import ContextDemo from './context/contextDemo';
+import useRefDemo from "./hooks/useRef/useRef";
+import ReducerDemo from "./hooks/userReducer/useReducer";
 
 const {Header, Sider, Content} = Layout;
 const {Item, SubMenu} = Menu;
@@ -22,13 +26,23 @@ function Homepage() {
               <span>Game</span>
               <Link to='/Game'/>
             </Item>
-            <Item key='myapp'>
+            <Item key='myApp'>
               <span>MyApp</span>
               <Link to='/MyApp'/>
             </Item>
-            <SubMenu key='hooks' title={
-              <span>HOOKS</span>
-            }>
+            <Item key='clock'>
+              <span>Clock</span>
+              <Link to='/Clock'/>
+            </Item>
+            <Item key='myClock'>
+              <span>myClock</span>
+              <Link to='/myClock'/>
+            </Item>
+            <Item key='/Context'>
+              <span>Context</span>
+              <Link to='/Content'/>
+            </Item>
+            <SubMenu key='hooks' title={<span>HOOKS</span>}>
               <Item key='useState'>
                 <span>useState</span>
                 <Link to='/hooks/useState'/>
@@ -36,6 +50,14 @@ function Homepage() {
               <Item key='useMemo'>
                 <span>useMemo</span>
                 <Link to='/hooks/useMemo'/>
+              </Item>
+              <Item key='useRef'>
+                <span>useRef</span>
+                <Link to='/hooks/useRef'/>
+              </Item>
+              <Item key='useReducer'>
+                <span>useReducer</span>
+                <Link to='/hooks/useReducer'/>
               </Item>
             </SubMenu>
           </Menu>
@@ -60,8 +82,13 @@ function Homepage() {
           >
             <Route path='/Game' component={Game}/>
             <Route path='/MyApp' render={() => <MyApp names={'I'}/>}/>
+            <Route path="/Clock" component={Clock}/>
+            <Route path="/MyClock" component={MyClock}/>
+            <Route path="/Context" component={ContextDemo}/>
             <Route path='/hooks/useState' component={useStateDemo}/>
             <Route path='/hooks/useMemo' component={useMemoDemo}/>
+            <Route path='/hooks/useRef' component={useRefDemo}/>
+            <Route path='/hooks/useReducer' component={ReducerDemo}/>
           </Content>
         </Layout>
       </Layout>
