ea0223cbe34f559f362b1ba785e6547fd54ff97d
whj768702 <whj768702@gmail.com>
2020-03-22 16:53:57 +0800

    feat(HOOSK): useRef增加说明和示例。

--- src/hooks/useRef/useRef.js
+++ src/hooks/useRef/useRef.js
@@ -1,28 +1,37 @@
 import React, {useRef, useState, useEffect} from 'react';
-
+/**
+ * 作用：获取DOM元素的节点
+        获取子组件的实例
+        渲染周期之间共享数据的存储（state不能存储跨渲染周期的数据，因为state的保存会触发组件重渲染）
+ */
 function useRefDemo() {
-  const inputEl = useRef(null);
-  const onButtonClick = () => {
-    inputEl.current.value = 'hello world!';
-    console.log(inputEl);
-  };
-
   const [text, setText] = useState('ce shi');
-  const textRef = useRef(null);
+  const textRef = useRef(text);
 
   useEffect(() => {
     textRef.current = text;
     console.log('terxtRef.current: ', textRef.current);
   });
+
+  const inputRef = useRef();
+  const onFocusClick = () => {
+    if (inputRef && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }
   return (
     <>
-      <input type="text" ref={inputEl}/>
-      <button onClick={onButtonClick}>在input上展示文字</button>
-      <br/>
-      <br/>
       <input type="text" value={text} onChange={(e) => {
         setText(e.target.value);
       }}/>
+      <br/>
+      <span>Ref保留组件上一次的状态:</span>
+      <span>{textRef.current}</span>
+      <br/>
+      <br/>
+      <span>操作DOM节点</span>
+      <input type="text" ref={inputRef}/>
+      <button onClick={onFocusClick}>点了聚焦input</button>
     </>
   );
 }
