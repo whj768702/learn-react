74927617aad4c181922f52fc9552f4e8bde17cc4
whj768702 <whj768702@gmail.com>
2020-03-28 19:32:13 +0800

    feat(useEffect): 补充信息。

--- src/hooks/useEffect/useEffectDemo.jsx
+++ src/hooks/useEffect/useEffectDemo.jsx
@@ -1,5 +1,10 @@
-import React, {useEffect, useState} from 'react';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { Button } from 'antd';
+
+/**
+ * 每次render或者re-render都会执行useEffect
+ */
 
 function Index() {
   useEffect(() => {
@@ -8,16 +13,18 @@ function Index() {
       console.log('index go');
     };
   }, []);
+
   return <h2>index</h2>;
 }
 
 function List() {
   useEffect(() => {
-    console.log('list page');
+    console.log('list page in');
     return () => {
       console.log('list go');
     };
   }, []);
+
   return <h2>list page</h2>;
 }
 
@@ -27,35 +34,50 @@ const UseEffectDemo = () => {
   /*
   effect中返回的一个方法起到解绑回收等操作。
   deps中是依赖，有对应依赖才会触发effect执行; 
-  useEffect(() =>{}): 无论哪个变化都会触发;
+  useEffect(() =>{}): 无论哪个跟踪的值变化都会触发;
   []: 只触发一次；
   [xxx]: xxx变化就会触发;
    */
   useEffect(() => {
     console.log(`useEffect => you clicked ${count} times`);
     return () => {
-      console.log('=========');
-    }
+      console.log('======回收函数执行了======');
+    };
   }, [count]);
+
+  useEffect(() => {
+    console.log('add mousemove event');
+    const onMouseMove = e => {
+      console.log(e);
+    };
+    window.addEventListener('mousemove', onMouseMove);
+    return () => {
+      console.log('mousemove event revmoved');
+      window.removeEventListener('mousemove', onMouseMove);
+    };
+  }, []);
 
   return (
     <div>
       <p>you clicked {count} times</p>
-      <button onClick={() => {
-        setCount(count + 1);
-      }}>click
-      </button>
+      <Button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        click
+      </Button>
       <BrowserRouter>
         <ul>
           <li>
-            <Link to='/'>index</Link>
+            <Link to="/">index</Link>
           </li>
           <li>
-            <Link to='/list'>list</Link>
+            <Link to="/list">list</Link>
           </li>
         </ul>
-        <Route path='/' exact component={Index}></Route>
-        <Route path='/list' component={List}></Route>
+        <Route path="/" exact component={Index}></Route>
+        <Route path="/list" component={List}></Route>
       </BrowserRouter>
     </div>
   );
