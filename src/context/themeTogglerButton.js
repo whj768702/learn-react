62ad4ff7745ee40ab8c278404f57184144a6c06f
whj768702 <whj768702@gmail.com>
2020-08-10 14:20:10 +0800

    feat(context): 按钮切换调整。

--- src/context/themeTogglerButton.js
+++ src/context/themeTogglerButton.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { ThemeContext } from './themeContext';
+import { Button } from 'antd';
 
 function ThemeTogglerButton() {
   return (
     <ThemeContext.Consumer>
       {(cxt) => (
-        <button onClick={cxt.toggleTheme} style={{ backgroundColor: cxt.theme.background }}>
+        <Button
+          onClick={cxt.toggleTheme}
+          style={{ backgroundColor: cxt.theme.background, color: cxt.theme.fontColor }}
+        >
           Toggle Theme 点击
-        </button>
+        </Button>
       )}
     </ThemeContext.Consumer>
   );
