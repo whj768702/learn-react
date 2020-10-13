10aa50e13f8e8d6097ce353064651ab1c0bd5422
whj768702 <whj768702@gmail.com>
2020-10-13 18:12:56 +0800

    feat(ant): 替换文件中的button为ant中组件Button.

--- src/hooks/useState/useState.jsx
+++ src/hooks/useState/useState.jsx
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
+
+import {Button} from "antd";
 
 const UseStateDemo = () => {
-  const [count, setCount] = useState(0);
-  return (
-    <div>
-      <p>clicked {count} times.</p>
-      <button onClick={()=>{setCount(count+1)}}>click</button>
-    </div>
-  );
-}
+    const [count, setCount] = useState(0);
+    return (
+        <div>
+            <p>clicked {count} times.</p>
+            <Button type="primary" size="middle" onClick={() => {
+                setCount(count + 1);
+            }}>click</Button>
+        </div>
+    );
+};
 
 export default UseStateDemo;
