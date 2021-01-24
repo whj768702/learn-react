20ab4a68a5ad4a7169003b3afcd87b0abd233cb3
whj768702 <whj768702@gmail.com>
2021-01-24 21:08:09 +0800

    style(文件): 格式化。

--- src/hooks/useMemo/useMemo.jsx
+++ src/hooks/useMemo/useMemo.jsx
@@ -1,42 +1,48 @@
-import React, {useState, useMemo} from 'react';
+import React, { useState, useMemo } from 'react';
 
-import {Button} from "antd";
+import { Button } from 'antd';
 
 function useMemoDemo() {
-    const [lisi, setLisi] = useState('李四等待中');
-    const [wangwu, setWangwu] = useState('王五等待中');
+  const [lisi, setLisi] = useState('李四等待中');
+  const [wangwu, setWangwu] = useState('王五等待中');
 
-    return (
-        <>
-            <Button onClick={() => {
-                setLisi(new Date().getTime());
-            }}>李四
-            </Button>
-            <Button onClick={() => {
-                setWangwu(new Date().getTime());
-            }}>王五
-            </Button>
-            <ChildComponent name={lisi}>{wangwu}</ChildComponent>
-        </>
-    );
+  return (
+    <>
+      <Button
+        onClick={() => {
+          setLisi(new Date().getTime());
+        }}
+      >
+        李四
+      </Button>
+      <Button
+        onClick={() => {
+          setWangwu(new Date().getTime());
+        }}
+      >
+        王五
+      </Button>
+      <ChildComponent name={lisi}>{wangwu}</ChildComponent>
+    </>
+  );
 }
 
-function ChildComponent({name, children}) {
-    function changeLisi(name) {
-        console.log('李四来了');
-        return name + '李四来了！！！';
-    }
+function ChildComponent({ name, children }) {
+  function changeLisi(name) {
+    console.log('李四来了');
+    return name + '李四来了！！！';
+  }
 
-    // 解决不必要的渲染，优化性能
-    // 第二个参数，name变化时，才重新渲染
-    const actionLisi = useMemo(() => changeLisi(name), [name]);
-    return (
-        <>
-            <div>{actionLisi}</div>
-            {/*<div>{changeLisi()}</div> 王五更新，李四每次也会打印，性能影响*/}
-            <div>{children}</div>
-        </>
-    );
+  // 解决不必要的渲染，优化性能
+  // 第二个参数，name变化时，才重新渲染
+  const actionLisi = useMemo(() => changeLisi(name), [name]);
+  return (
+    <>
+      <div>{actionLisi}</div>
+      {/*<div>{changeLisi()}</div> 王五更新，李四每次也会打印，性能影响*/}
+      <div>{children}</div>
+    </>
+  );
 }
 
 export default useMemoDemo;
