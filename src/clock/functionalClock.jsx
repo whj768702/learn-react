e6cf5d51fec47e0144fbe11ebd9e356fb57908b1
whj768702 <whj768702@gmail.com>
2021-02-05 15:28:57 +0800

    refactor(clock): 拆分文件。

---
+++ src/clock/functionalClock.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+const FunctionalClock = () => {
+    console.log('functional clock');
+    const [date, setDate] = useState(new Date());
+
+    useEffect(() => {
+        const timerID = setInterval(() => setDate(new Date()), 1000);
+        return function () {
+            clearInterval(timerID);
+        };
+    });
+    return (
+        <div>
+            <h1>函数式组件</h1>
+            <h2>it is {date.toLocaleString()}</h2>
+        </div>
+    );
+}
+export default FunctionalClock;
