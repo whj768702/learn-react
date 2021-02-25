fc7b468b83113c1bebb9404afad5262d0bdb68ce
whj768702 <whj768702@gmail.com>
2021-02-25 14:06:31 +0800

    feat(road2react): 增加表头降序排列功能。

--- src/road2react/List/List.jsx
+++ src/road2react/List/List.jsx
@@ -1,5 +1,30 @@
-import React, { memo } from 'react';
+import { sortBy } from 'lodash';
+import React, { memo, useState } from 'react';
+import '../road2react.css';
 
+const ColumnHeader = ({ onSort }) => {
+  const handleSort = (key) => {
+    onSort(key);
+  }
+
+  return (
+    <div style={{ display: 'flex' }}>
+      <span style={{ width: '40%' }}>
+        <button type='button' onClick={() => handleSort('TITLE')}>Title</button>
+      </span>
+      <span style={{ width: '30%' }}>
+        <button type='button' onClick={() => handleSort('AUTHOR')}>Author</button>
+      </span>
+      <span style={{ width: '10%' }}>
+        <button type='button' onClick={() => handleSort('COMMENT')}>Comments</button>
+      </span>
+      <span style={{ width: '10%' }}>
+        <button type='button' onClick={() => handleSort('POINT')}>Points</button>
+      </span>
+      <span style={{ width: '10%' }}>Actions</span>
+    </div>
+  );
+}
 const Item = ({ item, onRemoveItem }) => {
   return (
     <div className='item'>
@@ -17,7 +42,30 @@ const Item = ({ item, onRemoveItem }) => {
     </div>
   )
 };
+const SORTS = {
+  NONE: list => list,
+  TITLE: list => sortBy(list, 'title'),
+  AUTHOR: list => sortBy(list, 'author'),
+  COMMENT: list => sortBy(list, 'num_comments').reverse(),
+  POINT: list => sortBy(list, 'points').reverse(),
+};
+const List = ({ list, onRemoveItem }) => {
+  console.log('B:List');
+  const [sort, setSort] = useState('NONE');
 
-const List = ({ list, onRemoveItem }) => console.log('B:List') || list.map((item) => <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />)
+  const handleSort = (key) => {
+    setSort(key);
+  }
+  const sortFunction = SORTS[sort];
+  const sortedList = sortFunction(list);
+
+  return (
+    <div>
+      <ColumnHeader onSort={handleSort} />
+      {sortedList.map((item) => <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+      )}
+    </div>
+  )
+}
 
 export default memo(List);
