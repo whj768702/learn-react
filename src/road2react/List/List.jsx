c91c3550fbb21282a5f42d94c43e5ca5c4e71eea
whj768702 <whj768702@gmail.com>
2021-02-25 14:37:13 +0800

    feat(road2react): 增加升降序标志。

--- src/road2react/List/List.jsx
+++ src/road2react/List/List.jsx
@@ -2,24 +2,37 @@ import { sortBy } from 'lodash';
 import React, { memo, useState } from 'react';
 import '../road2react.css';
 
-const ColumnHeader = ({ onSort }) => {
+const SortArrow = ({ sort, currentKey }) => {
+  if (sort.sortKey === currentKey) {
+    return (<span>{sort.isReverse ? '升' : '降'}</span>);
+  } else {
+    return (<></>)
+  }
+}
+
+const ColumnHeader = ({ sort, onSort }) => {
   const handleSort = (key) => {
     onSort(key);
   }
+  console.log('sort: ', sort);
 
   return (
     <div style={{ display: 'flex' }}>
       <span style={{ width: '40%' }}>
-        <button type='button' onClick={() => handleSort('TITLE')}>Title</button>
+        <button type='button' onClick={() => handleSort('TITLE')}> Title </button>
+        <SortArrow sort={sort} currentKey='TITLE' />
       </span>
-      <span style={{ width: '30%' }}>
+      <span style={{ width: '20%' }}>
         <button type='button' onClick={() => handleSort('AUTHOR')}>Author</button>
+        <SortArrow sort={sort} currentKey='AUTHOR' />
       </span>
-      <span style={{ width: '10%' }}>
+      <span style={{ width: '20%' }}>
         <button type='button' onClick={() => handleSort('COMMENT')}>Comments</button>
+        <SortArrow sort={sort} currentKey='COMMENT' />
       </span>
       <span style={{ width: '10%' }}>
         <button type='button' onClick={() => handleSort('POINT')}>Points</button>
+        <SortArrow sort={sort} currentKey='POINT' />
       </span>
       <span style={{ width: '10%' }}>Actions</span>
     </div>
@@ -51,17 +64,21 @@ const SORTS = {
 };
 const List = ({ list, onRemoveItem }) => {
   console.log('B:List');
-  const [sort, setSort] = useState('NONE');
+  const [sort, setSort] = useState({
+    sortKey: 'NONE',
+    isReverse: false
+  });
 
   const handleSort = (key) => {
-    setSort(key);
+    const isReverse = sort.sortKey === key && !sort.isReverse;
+    setSort({ sortKey: key, isReverse: isReverse });
   }
-  const sortFunction = SORTS[sort];
-  const sortedList = sortFunction(list);
+  const sortFunction = SORTS[sort.sortKey];
+  const sortedList = sort.isReverse ? sortFunction(list).reverse() : sortFunction(list);
 
   return (
     <div>
-      <ColumnHeader onSort={handleSort} />
+      <ColumnHeader sort={sort} onSort={handleSort} />
       {sortedList.map((item) => <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
       )}
     </div>
