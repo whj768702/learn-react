0aa3c54de7dc5f793f3c93efc5031b1c096e4a2d
whj768702 <whj768702@gmail.com>
2023-02-21 10:49:54 +0800

    feat(react in action): 书籍信息展示

--- src/reactInAction/bookablesList.tsx
+++ src/reactInAction/bookablesList.tsx
@@ -17,8 +17,8 @@ const BookablesList = () => {
     setBookableIndex(i => (i + 1) % bookablesInGroup.length);
   }
 
-  const getUniqueValues = (arr, key)=> {
-    const propValues = arr.map(item=>item[key]);
+  const getUniqueValues = (arr, key) => {
+    const propValues = arr.map(item => item[key]);
     const uniqueValues = new Set(propValues);
 
     const uniqueValuesArr = [...uniqueValues];
@@ -26,25 +26,43 @@ const BookablesList = () => {
     return uniqueValuesArr;
   }
 
-  const groups = getUniqueValues(data.bookables, 'group').map(item=>({label:item, value:item}));
+  const groups = getUniqueValues(data.bookables, 'group').map(item => ({ label: item, value: item }));
 
-  const handleOnChange = (value)=> {
+  const handleOnChange = (value) => {
     setGroup(value);
   }
+
+  const bookable = bookablesInGroup[bookableIndex];
+  const [hasDetails, setHasDetails] = useState(false);
   return (
-    <>
-      <Select style={{width: 120}} onChange={handleOnChange} options={groups}></Select>
-      <div className='flex flex-col gap-y-1'>
-        {bookablesInGroup.map((b, i) => (
-          <span key={b.id} className={['w-fit', bookableIndex === i ? 'bg-red-600' : undefined].join(' ')} >
-            <Button onClick={() => changeBookable(i)}>{b.title}</Button>
-          </span>
-        ))}
+    <div className='flex justify-around'>
+      <div>
+        <Select style={{ width: 120 }} onChange={handleOnChange} options={groups}></Select>
+        <div className='flex flex-col gap-y-1'>
+          {bookablesInGroup.map((b, i) => (
+            <span key={b.id} className={['w-fit', bookableIndex === i ? 'bg-red-600' : undefined].join(' ')} >
+              <Button onClick={() => changeBookable(i)}>{b.title}</Button>
+            </span>
+          ))}
+        </div>
+        <div className='mt-2'>
+          <Button onClick={nextBookable}>Next</Button>
+        </div>
       </div>
-      <div className='mt-2'>
-        <Button onClick={nextBookable}>Next</Button>
-      </div>
-    </>
+      {bookable && (
+        <div>
+          <h3>{bookable.title}</h3>
+          <div>
+            <ul>
+              {bookable.days.sort().map(d => <li key={d}>{data.days[d]}</li>)}
+            </ul>
+            <ul>
+              {bookable.sessions.map(s => <li key={s}>{data.sessions[s]}</li>)}
+            </ul>
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
 
