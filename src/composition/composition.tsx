778cd713bac414b6e43480716d7fdae2b6001ec8
whj768702 <whj768702@gmail.com>
2024-08-16 09:38:34 +0800

    refactor(composition): 重命名文件并添加类型定义
    文件由composition.jsx重命名为composition.tsx，以反映添加了TypeScript定义。同时，为props引入了PropsType和ComponentType类型，以提升组件FancyBorder和SplitPane的可维护性和代码健壮性。修复了FancyBorder组件中props.children的拼写错误。

--- src/composition/composition.tsx
+++ src/composition/composition.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import type React from "react";
 
-function FancyBorder(props) {
+type PropsType = {
+  color: string;
+  children:
+    | string
+    | number
+    | boolean
+    | React.ReactElement
+    | Iterable<React.ReactNode>
+    | React.ReactPortal
+    | null
+    | undefined;
+};
+
+type ComponentType = {
+  left: React.ReactElement;
+  right: React.ReactElement;
+};
+
+function FancyBorder(props: PropsType) {
   return (
     <div className={`FancyBorder FancyBorder-${props.color}`}>
-      {props.childrend}
+      {props.children}
     </div>
   );
 }
@@ -11,17 +29,15 @@ function FancyBorder(props) {
 function WelcomeDialog() {
   return (
     <FancyBorder color="blue">
-      <h1 className="Dialog-title">
-        Welcome
-      </h1>
+      <h1 className="Dialog-title">Welcome</h1>
       <p className="Dialog-message">hello.</p>
     </FancyBorder>
   );
 }
 
-function SplitPane(props) {
+function SplitPane(props: ComponentType) {
   return (
-    <div className='mt-8 flex'>
+    <div className="mt-8 flex">
       <div className="left w-40">
         <span>左侧</span>
         {props.left}
@@ -54,9 +70,7 @@ function Example() {
   return (
     <>
       <h3>下面两个通过props传入组件</h3>
-      <SplitPane
-        left={<Contacts />}
-        right={<Chat />} />
+      <SplitPane left={<Contacts />} right={<Chat />} />
     </>
   );
 }
